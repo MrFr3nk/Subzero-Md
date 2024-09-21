fc007806d3430afc796fdd0819f529418e0e63b1
ᴹᴿ᭄ Fʀᴀɴᴋ ᴏғᴄ 々 <darrelmucheri@gmail.com>
2024-09-21 21:16:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ module.exports = {
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB || "mongodb+srv://darexmucheri:cMd7EoTwGglJGXwR@cluster0.uwf6z.mongodb.net/",
-    sessionId: process.env.SESSION_ID || "aDNoMDJ_Queen-Anya_zaGs=",
+    sessionId: process.env.SESSION_ID || "",
 
     /**
      * thumbnails and medias
